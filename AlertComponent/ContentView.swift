47d3f9da97522cb1acb649033d559760394a6040
Mickael MAS <mickael.mas@appiwedia.com>
2020-06-15 16:39:08 +0200

    Initial Commit

---
+++ AlertComponent/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AlertComponent
+//
+//  Created by Mickael Mas on 15/06/2020.
+//  Copyright © 2020 APPIWEDIA. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
